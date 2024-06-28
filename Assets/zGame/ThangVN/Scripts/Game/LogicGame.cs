545e61ea4d0396a4c14b0ee29765b093743bdb74
ThangVN <95681827+VuNgocThang@users.noreply.github.com>
2024-06-28 10:11:44 +0700

    ...

--- Assets/zGame/ThangVN/Scripts/Game/LogicGame.cs
+++ Assets/zGame/ThangVN/Scripts/Game/LogicGame.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.Rendering.VolumeComponent;
 
 [Serializable]
 public class ListSpawnPos
@@ -21,8 +20,9 @@ public class LogicGame : MonoBehaviour
     [SerializeField] BoomManager boomManager;
     public List<IBoom> listBoom;
 
-    [SerializeField] float timeSpawnBoomSpecial = -1f;
+    //[SerializeField] float timeSpawnBoomSpecial = -1f;
     [SerializeField] public static float timerCount;
+    [SerializeField] float totalTimeInWave = 3f;
 
     private void Start()
     {
@@ -43,34 +43,34 @@ public class LogicGame : MonoBehaviour
 
             if (!isSpawning)
             {
-                if (timerCount < 20f)
-                {
-                    StartCoroutine(SpawnBooms());
-                }
-                else
-                {
-                    StartCoroutine(SpawnBoomBig());
-                }
+                //if (timerCount < 20f)
+                //{
+                StartCoroutine(SpawnBooms());
+                //}
+                //else
+                //{
+                //    StartCoroutine(SpawnBoomBig());
+                //}
             }
         }
 
-        if (timeSpawnBoomSpecial > 0f)
-        {
-            timeSpawnBoomSpecial -= Ez.TimeMod;
-        }
-        else
-        {
-            timeSpawnBoomSpecial = 2f;
+        //if (timeSpawnBoomSpecial > 0f)
+        //{
+        //    timeSpawnBoomSpecial -= Ez.TimeMod;
+        //}
+        //else
+        //{
+        //    timeSpawnBoomSpecial = 2f;
 
-            if (timerCount > 20f)
-            {
-                DoubleBoom doubleBoom = boomManager.GetDoubleBoom();
-                Vector3 spawnPos = new Vector3(player.transform.position.x, 10f, 0f);
-                doubleBoom.Init(spawnPos);
-                listBoom.Add(doubleBoom);
-            }
+        //    if (timerCount > 20f)
+        //    {
+        //        DoubleBoom doubleBoom = boomManager.GetDoubleBoom();
+        //        Vector3 spawnPos = new Vector3(player.transform.position.x, 10f, 0f);
+        //        doubleBoom.Init(spawnPos);
+        //        listBoom.Add(doubleBoom);
+        //    }
 
-        }
+        //}
     }
 
     bool isSpawning;
@@ -79,49 +79,48 @@ public class LogicGame : MonoBehaviour
         int index1 = UnityEngine.Random.Range(0, listContaineListSpawn.Count);
 
         isSpawning = true;
+        int count = listContaineListSpawn[index1].listPos.Count;
 
-        for (int i = 0; i < listContaineListSpawn[index1].listPos.Count; i++)
+        for (int i = 0; i < count; i++)
         {
             SingleBoom singleBoom = boomManager.GetSingleBoom();
-            Vector3 spawnPos = new Vector3(listContaineListSpawn[index1].listPos[0].position.x + i * 3f, listContaineListSpawn[index1].listPos[0].position.y, 0);
+            Vector3 spawnPos = new Vector3(listContaineListSpawn[index1].startPos.position.x + i * 3f, listContaineListSpawn[index1].startPos.position.y, 0);
             singleBoom.Init(spawnPos);
             listBoom.Add(singleBoom);
 
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(totalTimeInWave / count);
         }
 
-        int index2 = UnityEngine.Random.Range(0, listContaineListSpawn.Count);
+        //int index2 = UnityEngine.Random.Range(0, listContaineListSpawn.Count);
 
-        for (int i = 0; i < listContaineListSpawn[index2].listPos.Count; i++)
-        {
-            DoubleBoom doubleBoom = boomManager.GetDoubleBoom();
-            Vector3 spawnPos = new Vector3(listContaineListSpawn[index2].listPos[0].position.x + i * 3f, listContaineListSpawn[index2].listPos[0].position.y, 0);
-            //Vector3 spawnPos = listContaineListSpawn[index2].listPos[i].position;
-            doubleBoom.Init(spawnPos);
-            listBoom.Add(doubleBoom);
+        //for (int i = 0; i < listContaineListSpawn[index2].listPos.Count; i++)
+        //{
+        //    DoubleBoom doubleBoom = boomManager.GetDoubleBoom();
+        //    Vector3 spawnPos = new Vector3(listContaineListSpawn[index2].startPos.position.x + i * 3f, listContaineListSpawn[index2].startPos.position.y, 0);
+        //    doubleBoom.Init(spawnPos);
+        //    listBoom.Add(doubleBoom);
 
-            yield return new WaitForSeconds(0.5f);
-        }
+        //    yield return new WaitForSeconds(0.5f);
+        //}
 
         isSpawning = false;
     }
 
-    private IEnumerator SpawnBoomBig()
-    {
-        int index = UnityEngine.Random.Range(0, listContaineListSpawn.Count);
+    //private IEnumerator SpawnBoomBig()
+    //{
+    //    int index = UnityEngine.Random.Range(0, listContaineListSpawn.Count);
 
-        isSpawning = true;
-        for (int i = 0; i < listContaineListSpawn[index].listPos.Count; i++)
-        {
-            BigBoom bigBoom = boomManager.GetBigBoom();
-            Vector3 spawnPos = new Vector3(listContaineListSpawn[index].listPos[0].position.x + i * 3f, listContaineListSpawn[index].listPos[0].position.y, 0);
-            bigBoom.Init(spawnPos);
-            listBoom.Add(bigBoom);
-
-            yield return new WaitForSeconds(0.7f);
-        }
-        isSpawning = false;
-    }
+    //    isSpawning = true;
+    //    for (int i = 0; i < listContaineListSpawn[index].listPos.Count; i++)
+    //    {
+    //        BigBoom bigBoom = boomManager.GetBigBoom();
+    //        Vector3 spawnPos = new Vector3(listContaineListSpawn[index].startPos.position.x + i * 3f, listContaineListSpawn[index].startPos.position.y, 0);
+    //        bigBoom.Init(spawnPos);
+    //        listBoom.Add(bigBoom);
+    //        yield return new WaitForSeconds(0.7f);
+    //    }
+    //    isSpawning = false;
+    //}
 
 
 
